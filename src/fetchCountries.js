33713dd4ef265d62492b7bdf4531c90a6eb89acb
TarasMysiura <tarasmysiura@gmail.com>
2023-05-16 18:32:38 +0300

    add

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,78 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+const debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.getElementById('search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryCard: document.querySelector('.country-info'),
+};
+
+refs.countryList.list_style = "none";
+
+refs.input.addEventListener('input', debounce(emptyName, DEBOUNCE_DELAY));
+
+function emptyName() {
+  if (refs.input.value.trim() === '') {
+    refs.countryList.innerHTML = '';
+    refs.countryCard.innerHTML = '';
+  } else {
+    search();
+  }
+}
+
+function search() {
+  fetchCountries(refs.input.value.trim())
+    .then(result => {
+      if (result.length > 10) {
+        handleTooManyMatchesFound();
+      } else if (result.length >= 2 && result.length <= 10) {
+        changeCountryList(result);
+        console.log(1);
+      } else if (result.length === 1) {
+        createCountryCard(result);
+        console.log(2);
+      } else {
+        Notiflix.Notify.failure('Oops, something went wronga!');
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, something went wrong!');
+    //   console.error(error);
+    });
+}
+
+function handleTooManyMatchesFound() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
+
+function changeCountryList(data) {
+  refs.countryList.innerHTML = ``;
+  refs.countryList.innerHTML = data
+    .map(({ flags, name }) => {
+      return `<li class="list-item" display="flex">
+  <img src="${flags.svg}" width="50" height="30" alt="${flags.alt}">
+  <h2 class="item-title">${name.official}</h2>
+</li>`;
+    })
+    .join('');
+  refs.countryCard.innerHTML = '';
+}
+
+function createCountryCard(data) {
+  refs.countryCard.innerHTML = data
+    .map(({ capital, flags, languages, name, population }) => {
+      return `<img src="${flags.svg}" width="70" height="50" alt="${flags.alt}">
+<h2>${name.common} (${name.official})</h2>
+<p>Capital: ${capital}</p>
+<p>Population: ${population.toLocaleString()}</p>
+<p>Languages: ${Object.values(languages).join(', ')}</p>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = '';
+}
